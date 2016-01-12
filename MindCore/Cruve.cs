6a5b89a9e0bf8ea20fa3040addf9e59ddc0e0fc1
pengfei zhang <pzhang11@stevens.edu>
2016-01-11 21:21:33 -0500

    init push

---
+++ MindCore/Cruve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindCore
+{
+    class Cruve : IDrawable
+    {
+        public void draw()
+        {
+           // throw new NotImplementedException();
+        }
+    }
+}
